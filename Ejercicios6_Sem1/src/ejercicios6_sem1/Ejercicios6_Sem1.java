415c7cc9e372ebaca96f499b39bee2307870e8a2
markgarcia1011 <101343926+markgarcia1011@users.noreply.github.com>
2022-04-01 18:43:11 +0200

    Update Ejercicios6_Sem1.java

--- Ejercicios6_Sem1/src/ejercicios6_sem1/Ejercicios6_Sem1.java
+++ Ejercicios6_Sem1/src/ejercicios6_sem1/Ejercicios6_Sem1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicios6_sem1;
 
 public class Ejercicios6_Sem1 {
